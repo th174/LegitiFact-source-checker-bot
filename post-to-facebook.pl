44867bfa31790ab632540c1f22e7c51ef7193548
Tianyue Huang <th174@duke.edu>
2016-11-19 08:04:20 -0500

    bot-chan is live!

--- post-to-facebook.pl
+++ post-to-facebook.pl
@@ -20,8 +20,8 @@ foreach (@{${$feed}{data}}){
     my $curr = $fb->query->find(${$_}{id})->select_fields(qw(id name link))->request->as_hashref;
     print ${$_}{id},"\n";
     print Dumper $curr;
-    if (${$curr}{link} =~ /^https?:\/\/www\.(.*)\.(com|org|net|\.uk|\.net|\.ca)\// && $1 !~ "facebook"){
-        $fb->add_comment(${$_}{id})->set_message("Hiya, I'm Bot-chan, and this is a test post! It looks like you've linked to www.$1.$2. Sorry, I'm not experienced enough yet to know if this is a good source to be linking, but I'm working really really hard on it! Someday, I'll be a real bot, and we'll have lots of fun together! Until then, wish me luck~~<3")->publish;
+    if (${$curr}{link} =~ /^https?:\/\/(www\.)?(.*)\.(com|org|net|\.uk|\.net|\.ca)\// && $2 !~ "facebook"){
+        $fb->add_comment(${$_}{id})->set_message("Hiya, I'm Bot-chan, and this is a test post! It looks like you've linked to www.$2.$3. Sorry, I'm not experienced enough yet to know if this is a good source to be linking, but I'm working really really hard on it! Someday, I'll be a real bot, and we'll have lots of fun together! Until then, wish me luck~~<3")->publish;
     }
     print "\n--------------------------------\n\n"
 }
