f7d30858753a3753c12f7dc3c36107b866e71b8a
Soravit Sophastienphong <ss833@duke.edu>
2016-11-20 00:29:02 -0500

    ;aoiewsfhopwesjp'j

--- Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/AppUsageActivity.java
+++ Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/AppUsageActivity.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
@@ -23,7 +24,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * Created by Soravit on 11/19/2016.
@@ -31,24 +35,31 @@ import java.util.List;
 
 public class AppUsageActivity extends AppCompatActivity {
 
+    UsageStatsManager mUsageStatsManager;
 
-    /** first app user */
+    /**
+     * first app user
+     */
     public static final int AID_APP = 10000;
 
-    /** offset for uid ranges for each user */
+    /**
+     * offset for uid ranges for each user
+     */
     public static final int AID_USER = 100000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appusage);
+        //Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        //startActivity(intent);
         final Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 AsyncTask.execute(new Runnable() {
                     @Override
                     public void run() {
-                        while(true) {
+                        while (true) {
                             printCurrentActivity();
                         }
                     }
@@ -57,23 +68,37 @@ public class AppUsageActivity extends AppCompatActivity {
         });
     }
 
-    public void printCurrentActivity(){
-        Log.d("App: ",getForegroundApp());
+    public void printCurrentActivity() {
+        UsageStats usageStats;
+
+        String PackageName = "Nothing";
+
+        long TimeInforground = 500;
+
+        int minutes = 500, seconds = 500, hours = 500;
+        UsageStatsManager mUsageStatsManager = (UsageStatsManager) getSystemService("usagestats");
+
+        long time = System.currentTimeMillis();
+
+        List<UsageStats> stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_YEARLY, time - 1000 * 10, time);
+
+        if (stats != null) {
+            SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
+            for (UsageStats usagestats : stats) {
+
+                TimeInforground = usagestats.getTotalTimeInForeground();
+
+                PackageName = usagestats.getPackageName();
+
+                minutes = (int) ((TimeInforground / (1000 * 60)) % 60);
+
+                seconds = (int) (TimeInforground / 1000) % 60;
+
+                hours = (int) ((TimeInforground / (1000 * 60 * 60)) % 24);
+
+                Log.d("BAC", "PackageName is" + PackageName + "Time is: " + hours + "h" + ":" + minutes + "m" + seconds + "s");
+
+            }
+        }
     }
-
-    public List<UsageStats> getUsageStatistics(int intervalType) {
-        // Get the app statistics since one year ago from the current time.
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.YEAR, -1);
-
-        List<UsageStats> queryUsageStats = mUsageStatsManager
-                .queryUsageStats(intervalType, cal.getTimeInMillis(),
-                        System.currentTimeMillis());
-
-        return queryUsageStats;
-    }
-
-
-
-
 }
