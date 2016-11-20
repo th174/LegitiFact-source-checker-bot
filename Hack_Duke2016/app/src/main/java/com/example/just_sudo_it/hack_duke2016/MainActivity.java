900d77ed70f3455078628abdc432fb4966c96909
Soravit Sophastienphong <ss833@duke.edu>
2016-11-19 20:25:41 -0500

    Pushed

--- Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/MainActivity.java
+++ Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.just_sudo_it.hack_duke2016;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            Intent intent = new Intent(this, AppUsageActivity.class);
+            startActivity(intent);
             return true;
         }
 
