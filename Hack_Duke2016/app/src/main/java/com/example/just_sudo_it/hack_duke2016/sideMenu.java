ab9cccd4f30644d228eeada5de2a2351c826d405
Pratiksha Sharma <ps179@duke.edu>
2016-11-19 21:03:25 -0500

    Added Graph

--- Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/sideMenu.java
+++ Hack_Duke2016/app/src/main/java/com/example/just_sudo_it/hack_duke2016/sideMenu.java
@@ -13,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class sideMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -40,6 +44,22 @@ public class sideMenu extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+
+        graph.addSeries(series);
+
     }
 
     @Override
@@ -98,4 +118,7 @@ public class sideMenu extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
 }
