784598d1bb28a3775dd8aff8ff131551a90bb2c0
Tianyue Huang <th174@duke.edu>
2016-11-21 18:23:35 -0500

    implemented logs correctly

--- run-source-checker.pl
+++ run-source-checker.pl
@@ -11,20 +11,24 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =localtime(time);
 printf ("***********************************************************************************\nStarted at %02d:%02d:%02d on %02d/%02d/%04d\n***********************************************************************************\n",$hour,$min,$sec,$mon,$mday,$year+1900);
 
 #Authenticate with Facebook
-my $access_token = 'EAASqp8MKvc8BADlwToVekM9NgIhrn08epXTB3LZCwOwRpnb1dcS8QhZB6bVxmnRRZAbTLaV8zsU9apjCQiFoZB5J4lNZBsgx6QzyWpZAcwntjs0KVT0n7QEpDipZBQorhAptU33x6WIidLpJXgNwdMWidGuZCHVZBB2me6FgEqW9ZCxAZDZD';
-my $fb = Facebook::Graph->new(
-    app_id => '1313537415364047',
-    secret => '9102f0d32b856d3b0bf520ea6259f631');
+my $access_token = 'EAAFvSiB8iKsBAMXhySQ4Ctk4TExWkoKua7HdZA421uxXsOHkrs2ZAmiygOcnrtJrd8enVGwSPqN2xzw6NphZC3217EPfX4idea0xjG0JYjIiomhJ7MMCQl077H67PPbOVPMfFBA2AByAau5lFS7IdUh6KZAkbEfhgoSvdDhjqEXCYMqIMO7K';
+my $fb = Facebook::Graph->new;
 $fb->access_token($access_token);
 $fb->authorize->extend_permissions(qw(publish_stream read_stream));
 my $page = $fb->fetch('/legitifact');
 
-my %sourcepages; #hash of Facebook page -> feed from last $time min
-my $time = $ARGV[1]; #minutes
+my %sourcepages; #perlhash of Facebook page -> feed from last $time min
+my $time = $ARGV[0]; #minutes
 
+if (!$time){
+    $time = 5;
+}
+printf "Querying souces from the past $time minutes....\n";
 $sourcepages{"Opposing Views"} = ${$fb->query->find("/${$fb->fetch('/opposingviews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Occupy Democrats"} = ${$fb->query->find("/${$fb->fetch('/OccupyDemocrats')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Huffington Post"} = ${$fb->query->find("/${$fb->fetch('/HuffingtonPost')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"HuffPost Politics"} = ${$fb->query->find("/${$fb->fetch('/HuffPostPolitics')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"HuffPost Women"} = ${$fb->query->find("/${$fb->fetch('/HuffPostWomen')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Wall Street Journal"} = ${$fb->query->find("/${$fb->fetch('/wsj')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Upworthy"} = ${$fb->query->find("/${$fb->fetch('/Upworthy')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Young Turks"} = ${$fb->query->find("/${$fb->fetch('/TheYoungTurks')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
@@ -35,6 +39,9 @@ $sourcepages{"BuzzFeed"} = ${$fb->query->find("/${$fb->fetch('/BuzzFeedVideo')}{
 $sourcepages{"The New York Times"} = ${$fb->query->find("/${$fb->fetch('/nytimes')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Mic"} = ${$fb->query->find("/${$fb->fetch('/MicMedia')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"CNN"} = ${$fb->query->find("/${$fb->fetch('/CNN')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"CNNMoney"} = ${$fb->query->find("/${$fb->fetch('/CNNMoney')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"CNN International"} = ${$fb->query->find("/${$fb->fetch('/CNNInternational')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"CNN Politics"} = ${$fb->query->find("/${$fb->fetch('/CNNPolitics')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"New York Magazine"} = ${$fb->query->find("/${$fb->fetch('/NewYorkMag')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"ATTN:"} = ${$fb->query->find("/${$fb->fetch('/attn')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Other 98%"} = ${$fb->query->find("/${$fb->fetch('/TheOther98')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
@@ -49,6 +56,7 @@ $sourcepages{"NBC News"} = ${$fb->query->find("/${$fb->fetch('/NBCNews')}{id}/fe
 $sourcepages{"NowThis"} = ${$fb->query->find("/${$fb->fetch('/NowThisNews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Guardian"} = ${$fb->query->find("/${$fb->fetch('/theguardian')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"VICE"} = ${$fb->query->find("/${$fb->fetch('/VICE')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"VICE News"} = ${$fb->query->find("/${$fb->fetch('/VICEnews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Daily Caller"} = ${$fb->query->find("/${$fb->fetch('/DailyCaller')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"POLITICO"} = ${$fb->query->find("/${$fb->fetch('/politico')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"the American Center for Law and Justice"} = ${$fb->query->find("/${$fb->fetch('/theACLJ')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
@@ -67,27 +75,35 @@ $sourcepages{"Glenn Beck"} = ${$fb->query->find("/${$fb->fetch('/GlennBeck')}{id
 $sourcepages{"Sean Hannity"} = ${$fb->query->find("/${$fb->fetch('/SeanHannity')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"USA TODAY"} = ${$fb->query->find("/${$fb->fetch('/usatoday')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"ABC News"} = ${$fb->query->find("/${$fb->fetch('/ABCNews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"ABC News Politics"} = ${$fb->query->find("/${$fb->fetch('/ABCNewsPolitics')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Rush Limbaugh"} = ${$fb->query->find("/${$fb->fetch('/RushLimbaugh')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"PBS"} = ${$fb->query->find("/${$fb->fetch('/pbs')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"PBS News Hour"} = ${$fb->query->find("/${$fb->fetch('/newshour')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Bloomberg"} = ${$fb->query->find("/${$fb->fetch('/bloombergbusiness')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"Mother Jones"} = ${$fb->query->find("/${$fb->fetch('/motherjones')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"The Ed Schultz Show"} = ${$fb->query->find("/${$fb->fetch('/edschultzshow')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 $sourcepages{"ThinkProgress"} = ${$fb->query->find("/${$fb->fetch('/thinkprogress')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"the Business Insider"} = ${$fb->query->find("/${$fb->fetch('/businessinsider')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"Newsweek"} = ${$fb->query->find("/${$fb->fetch('/newsweek')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"Breaking News"} = ${$fb->query->find("/${$fb->fetch('/BreakingNews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"the AP"} = ${$fb->query->find("/${$fb->fetch('/APNews')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"Reuters"} = ${$fb->query->find("/${$fb->fetch('/Reuters')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
+$sourcepages{"CNBC"} = ${$fb->query->find("/${$fb->fetch('/cnbc')}{id}/feed")->select_fields(qw(id name link message created_time))->where_since("-$time minutes")->request->as_hashref}{data};
 
 #load info froms tsv file
-open(my $research, "< news-sites-research.txt") or die("could not open txt");
+open(my $research, "< /home/th174/Documents/Legitifact-source-checker-bot/news-sites-research.txt") or die("could not open datafile");
 my %websites;
 
 while(<$research>){
-    if($_ =~ /^(Known)?\t(.+)\t(http(s)?:\/\/)?(www\.)?(.*\.(com|org|co.uk).+?)\t(Left|Right|Unbiased|Far Left|Far Right)\t(more distrusted than trusted|more trusted than distrusted|about equally trusted and distrusted)/){
-#         print $_,"\n";
+    if($_ =~ /^(Known)?\t(.+)\t(http(s)?:\/\/)?(www\.)?(.+?)(\/.*)?\t(Left|Right|Unbiased|Far Left|Far Right)\t(more distrusted than trusted|more trusted than distrusted|about equally trusted and distrusted)$/){
+        #         print $_,"\n";
         $websites{$6} = {
             url => $6,
             popularity => $1,
             source_name => $2,
             bias => $8,
             credibility => $9,
-            };
+        };
     }
 }
 close ($research);
@@ -96,23 +112,38 @@ foreach my $source (keys %sourcepages){
     print "\n$source\n-----------------------------\n";
     foreach (@{$sourcepages{$source}}) {
         my $url = ${$_}{link};
-#         print Dumper $_;
-        if ($url =~ /^(http(s)?:\/\/)?(www\.)?(.*\.(com|org|co.uk).+?)/ && $websites{$4}){
-            print "Domain: $4\n";
-            print Dumper $websites{$4};
-            my $message = "The Facebook page of $source originally shared this article. According to data gathered by the Pew Research Center, $websites{$4}{source_name} is $websites{$4}{credibility} by survey respondents who have heard of the source. ";
-            if ($websites{$4}{popularity}){
-                $message = $message."(Over 40% of respondents) ";
+        print Dumper $_;
+        if ($url && $url =~ /^(http(s)?:\/\/)?(www\.)?(.+?)\/.*$/){
+            my $domain = $4;
+            print "\nDomain: $domain\n";
+            if ($domain =~ /cnnmon/) {
+                $domain = 'cnn.it';
             }
-            if ($websites{$4}{bias} =~ /Left|Right/){
-                $message = $message."\nAdditionally, the research indicates that evaluations of this source's credibility varies among readers with different political views. ";
-            } elsif($websites{$4}{bias}=~/Far Left/){
-                $message = $message."\nAdditionally, the research indicates that left-leaning readers were much more likely to find this source credible than other readers. ";
-            } elsif($websites{$4}{bias}=~/Far Right/){
-                $message = $message."\nAdditionally, the research indicates that right-leaning readers were much more likely to find this source credible than other readers. ";
+            if ($domain =~ /foxnews/) {
+                $domain = 'foxnews.com';
             }
-            $message = $message."\n\nTo learn more about Pew Research Center's study on Political Polarization & Media Habits, follow this link to visit their report. https://goo.gl/xwVtjv";
-            $fb->add_page_feed->set_page_id(${$page}{id})->set_message($message)->set_link_uri("$url")->publish;
+#             if ($domain =~ /trib.al/) {
+#                 $domain = 'breitbart.com';
+#             }
+            if ($domain =~ /buff.ly/) {
+                $domain = 'theblaze.com';
+            }
+            if ($websites{$domain}){
+                my $message = "The Facebook page of $source originally shared this article. According to data gathered by the Pew Research Center, $websites{$domain}{source_name} is $websites{$domain}{credibility} by survey respondents who have heard of the source. ";
+                if ($websites{$domain}{popularity}){
+                    $message = $message."(Over 40% of respondents) ";
+                }
+                if ($websites{$domain}{bias} =~ /Left|Right/){
+                    $message = $message."\nAdditionally, the research indicates that evaluations of this source's credibility varies among readers with different political views. ";
+                } elsif($websites{$domain}{bias}=~/Far Left/){
+                    $message = $message."\nAdditionally, the research indicates that left-leaning readers were much more likely to find this source credible than other readers. ";
+                } elsif($websites{$domain}{bias}=~/Far Right/){
+                    $message = $message."\nAdditionally, the research indicates that right-leaning readers were much more likely to find this source credible than other readers. ";
+                }
+                $message = $message."\n\nTo learn more about Pew Research Center's study on Political Polarization & Media Habits, follow this link to visit their report. https://goo.gl/xwVtjv";
+                print Dumper $fb->add_page_feed->set_page_id(${$page}{id})->set_message("$message")->set_link_uri("$url")->publish;
+                #                 print $message,"\n";
+            }
         }
     }
 }
